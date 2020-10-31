ba12eb14f79e9e292edfcf67ad8c4adcd6a44ca0
serialagony <serialagony31@yahoo.com>
2020-10-31 17:38:44 -0400

    adding migrations

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['serialagony31@yahoo.com']
+    POSTS_PER_PAGE= 3
+    LANGUAGES = ['en']
+    MS_TRANSLATOR_KEY = '177bd5f38e004bb38455431bc8dec346'
+    # MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = 'http://localhost:9200'
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
